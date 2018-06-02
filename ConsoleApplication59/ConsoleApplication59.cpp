70f6852cd2548bcee555c4cc14b3a5b460f1b3d5
takab <takab@LAPTOP-LTSQ7CE3>
2018-06-02 20:07:09 +0600

    Add project files.

---
+++ ConsoleApplication59/ConsoleApplication59.cpp
@@ -0,0 +1,31 @@
+// ConsoleApplication59.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "MyString.h"
+#include "MyVector.h"
+
+int main()
+{
+	Vector victim;
+
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+	victim.add(MyString("ConsoleApplication10.cpp : Defines the entry point for the console application."));
+
+	victim.ToString();
+
+
+    return 0;
+}
+
---
+++ ConsoleApplication59/MyString.h
@@ -0,0 +1,14 @@
+#pragma once
+class MyString
+{
+private:
+	char *MyChar;
+public:
+	MyString();
+	MyString(int count);
+	explicit MyString(const char * str);
+	MyString(const MyString & obj);
+	void ToString();
+	void Copy(const char *str);
+	~MyString();
+};
